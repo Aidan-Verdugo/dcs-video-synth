62b9b877f20a11438813e7ad35e43e8c3e8acbe8
Aidan-Verdugo <40747347+Aidan-Verdugo@users.noreply.github.com>
2023-10-18 15:32:46 -0500

    made the test player

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("test")
+import vlc
+import time
+
+def audio(source):
+    vlc_instance = vlc.Instance()
+
+    player = vlc_instance.media_player_new()
+
+    media = vlc_instance.media_new(source)
+
+    player.set_media(media)
+
+    player.play()
+
+    time.sleep(0.5)
+
+    duration = player.get_length()
+
+    print("Duration : " + str(duration))
+
+audio("~/Music/sleepyhead.mp3")
+
+

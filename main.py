1389a881228d12834dfc8ce09ce22d67eac01068
Aidan-Verdugo <40747347+Aidan-Verdugo@users.noreply.github.com>
2023-10-18 15:58:58 -0500

    third time's the charm

--- main.py
+++ main.py
@@ -1,34 +1,7 @@
-from vlc import Instance
-import time
-import os
+import vlc
 
-class VLC:
-    def __init__(self):
-        self.Player = Instance('--loop')
+media = vlc.MediaPlayer("Playlist.m3u")
 
-    def addPlaylist(self):
-        self.mediaList = self.Player.media_list_new()
-        path = r"~/Music/Playlist1.m3u"
-        songs = os.listdir(path)
-        for s in songs:
-            self.mediaList.add_media(self.Player.media_new(os.path.join(path,s)))
-        self.listPlayer = self.Player.media_list_player_new()
-        self.listPlayer.set_media_list(self.mediaList)
-    def play(self):
-        self.listPlayer.play()
-    def next(self):
-        self.listPlayer.next()
-    def pause(self):
-        self.listPlayer.pause()
-    def previous(self):
-        self.listPlayer.previous()
-    def stop(self):
-        self.listPlayer.stop()
-
-player = VLC()
-
-player.addPlaylist()
-
-player.play()
+media.play
 
 
